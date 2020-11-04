f6dcf95322dc6e047b5e0bb790d49b4de646f1fa
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2020-11-04 17:34:03 -0300

    vector2 ahora extiende java.awt.point

--- src/GUI/Vector2.java
+++ src/GUI/Vector2.java
@@ -1,38 +1,21 @@
 package GUI;
 
+import java.awt.Point;
+
 /**
  * Modela un vector en R^2
  */
-public class Vector2 {
-    private double vectorX;
-    private double vectorY;
-
-    public Vector2(double x, double y) {
-        vectorX = x;
-        vectorY = y;
-    }
-
-    public double getX() {
-        return vectorX;
-    }
-
-    public double getY() {
-        return vectorY;
-    }
-
-    public void setX(double x) {
-        vectorX = x;
-    }
-
-    public void setY(double y) {
-        vectorY = y;
+public class Vector2 extends Point {
+    public Vector2(int x, int y) {
+        super(x, y);
     }
 
     /**
      * Suma las componentes x e y de dos vectores
      */
     public void add(Vector2 v) {
-        vectorX += v.getX();
-        vectorY += v.getY();
+        double x = this.getX() + v.getX();
+        double y = this.getY() + v.getY();
+        this.setLocation(x, y);
     }
 }
--- src/proyectil/Proyectil.java
+++ src/proyectil/Proyectil.java
@@ -1,8 +1,11 @@
 package proyectil;
 
+import javax.swing.JLabel;
+
+import GUI.ImageProvider;
 import GUI.Vector2;
 
-public abstract class Proyectil {
+public abstract class Proyectil extends JLabel{
     protected int daño;
     protected Vector2 posicion;
     protected Vector2 velocidad;
@@ -11,13 +14,19 @@ public abstract class Proyectil {
         this.daño = daño;
         this.posicion = posicion;
         this.velocidad = velocidad;
+        this.setIcon(ImageProvider.getInstancia().getSpriteProyectil());
     }
 
     public void update() {
         posicion.add(velocidad);
+        this.setLocation(posicion);
     }
 
     public int getDaño() {
         return daño;
+    }
+
+    public Vector2 getPosicion(){
+        return posicion;
     }
 }
