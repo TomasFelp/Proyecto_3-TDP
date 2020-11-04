38ff5c4040ed07da651248d6a617893813a685a8
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2020-11-04 18:37:03 -0300

    ahora la tecla w acciona la funcion de disparar del arma del jugador

--- src/cerebros/ComandoPlayer.java
+++ src/cerebros/ComandoPlayer.java
@@ -5,19 +5,17 @@ import entidades.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import juego.*;
+
 public class ComandoPlayer extends Thread implements KeyListener{
-//Attributes
 	protected Player player;
 	protected JFrameJuego jfJuego;
 	protected boolean ejecucion=true;
-	
-//Builder
+
 	public ComandoPlayer(Player p,JFrameJuego j) {
 		player=p;
 		jfJuego=j;
 	}
-//Methods
-	
+
 	public Player getPlayer() {
 		return player;
 	}
@@ -25,26 +23,28 @@ public class ComandoPlayer extends Thread implements KeyListener{
 @Override
 public void keyTyped(KeyEvent e) {
 	// TODO Auto-generated method stub
-	
 }
 
 /**
  * Controla la direccion en la que se mueve el personaje.
  */
 public void keyPressed(KeyEvent e) {
-	// TODO Auto-generated method stub
-	int teclaPrecionada=e.getKeyCode();
+	int teclaPresionada=e.getKeyCode();
 	Point posicion=this.player.getLocation();
 
-	if(teclaPrecionada==KeyEvent.VK_A) {
+	if(teclaPresionada==KeyEvent.VK_A) {
 		if(posicion.x>0)
 			posicion.setLocation(posicion.x-player.getVelocidad(),posicion.y);
 	}
-	
-	if(teclaPrecionada==KeyEvent.VK_D) {
+
+	if(teclaPresionada==KeyEvent.VK_D) {
 		if(posicion.x<jfJuego.getAncho()-30)
 			posicion.setLocation(posicion.x+player.getVelocidad(),posicion.y);
 	}
+
+  if(teclaPresionada == KeyEvent.VK_W) {
+      this.player.disparar();
+  }
 
 	player.setLocation(posicion);
 	jfJuego.repaint();
