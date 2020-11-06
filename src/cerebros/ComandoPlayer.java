97d28d4b4679ec4bb7521c6223299c749dc17d76
Facundo Alvarado <facundoalvarado9@gmail.com>
2020-11-06 16:29:37 -0300

    Ahora el player puede moverse con flechas, y corregi indentacion

--- src/cerebros/ComandoPlayer.java
+++ src/cerebros/ComandoPlayer.java
@@ -20,50 +20,50 @@ public class ComandoPlayer extends Thread implements KeyListener{
 		return player;
 	}
 
-@Override
-public void keyTyped(KeyEvent e) {
-	// TODO Auto-generated method stub
-}
-
-/**
- * Controla la direccion en la que se mueve el personaje.
- */
-public void keyPressed(KeyEvent e) {
-	int teclaPresionada=e.getKeyCode();
-	Point posicion=this.player.getLocation();
-
-	if(teclaPresionada==KeyEvent.VK_A) {
-		if(posicion.x>0)
-			posicion.setLocation(posicion.x-player.getVelocidad(),posicion.y);
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 	}
 
-	if(teclaPresionada==KeyEvent.VK_D) {
-		if(posicion.x<jfJuego.getAncho()-30)
-			posicion.setLocation(posicion.x+player.getVelocidad(),posicion.y);
+	/**
+	 * Controla la direccion en la que se mueve el personaje.
+	 */
+	public void keyPressed(KeyEvent e) {
+		int teclaPresionada=e.getKeyCode();
+		Point posicion=this.player.getLocation();
+
+		if(teclaPresionada==KeyEvent.VK_A || teclaPresionada==KeyEvent.VK_LEFT) {
+			if(posicion.x>0)
+				posicion.setLocation(posicion.x-player.getVelocidad(),posicion.y);
+		}
+
+		if(teclaPresionada==KeyEvent.VK_D || teclaPresionada==KeyEvent.VK_RIGHT) {
+			if(posicion.x<jfJuego.getAncho()-30)
+				posicion.setLocation(posicion.x+player.getVelocidad(),posicion.y);
+		}
+
+	  if(teclaPresionada == KeyEvent.VK_W || teclaPresionada==KeyEvent.VK_UP) {
+		  this.player.disparar();
+	  }
+
+		player.setLocation(posicion);
+		jfJuego.repaint();
 	}
 
-  if(teclaPresionada == KeyEvent.VK_W) {
-      this.player.disparar();
-  }
 
-	player.setLocation(posicion);
-	jfJuego.repaint();
-}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 
+	}
 
-@Override
-public void keyReleased(KeyEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	public void setPlayer(Player p) {
+		player=p;
+	}
 
-public void setPlayer(Player p) {
-	player=p;
-}
-
-public void setJFrameJuego(JFrameJuego j) {
-	jfJuego=j;
-}
+	public void setJFrameJuego(JFrameJuego j) {
+		jfJuego=j;
+	}
 
 
 }
